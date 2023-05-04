0f612cbfb7cec01ac72c23d0a9522bdb1fbefa14
Chen001117 <cwz19@mails.tsinghua.edu.cn>
2023-05-04 17:13:05 +0800

    fix "rewards not found" bug

--- openrl/rewards/base_reward.py
+++ openrl/rewards/base_reward.py
@@ -12,29 +12,14 @@ class BaseReward(object):
     def step_reward(
         self, data: Dict[str, Any]
     ) -> Union[np.ndarray, List[Dict[str, Any]]]:
-        rewards = data["reward"].copy()
-        infos = []
-
-        for rew_func in self.step_rew_funcs.values():
-            new_rew, new_info = rew_func(data)
-            if len(infos) == 0:
-                infos = new_info
-            else:
-                for i in range(len(infos)):
-                    infos[i].update(new_info[i])
-            rewards += new_rew
+        
+        rewards = data["rewards"].copy()
+        infos = [dict() for _ in range(rewards.shape[0])]
 
         return rewards, infos
 
     def batch_rewards(self, buffer: Any) -> Dict[str, Any]:
+        
         infos = dict()
 
-        for rew_func in self.batch_rew_funcs.values():
-            new_rew, new_info = rew_func()
-            if len(infos) == 0:
-                infos = new_info
-            else:
-                infos.update(new_info)
-        # update rewards, and infos here
-
-        return dict()
+        return infos
