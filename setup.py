6ec1962d5643330e5ebe7eaf31a90976e10c8f59
ChildTang <1260007864@qq.com>
2023-05-16 22:26:08 +0800

    fix setup error

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def get_extra_requires() -> dict:
             "black",
             "ruff",
             "gpustat",
-            # "gym-retro",
+            "gym-retro",
         ],
         "dev": ["build", "twine"],
         "mpe": ["pyglet==1.5.27"],
