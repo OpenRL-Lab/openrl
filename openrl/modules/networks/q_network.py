2a19fe8e9dbace82f59685f73625037536530d23
unknown <yzl54d1123@163.com>
2023-05-17 21:29:16 +0800

    Update networks for off-policy alg

--- openrl/modules/networks/q_network.py
+++ openrl/modules/networks/q_network.py
@@ -16,18 +16,19 @@
 
 """"""
 import torch
+import torch.nn as nn
 
-from openrl.buffers.utils.util import get_policy_obs, get_policy_obs_space
-from openrl.modules.networks.base_policy_network import BasePolicyNetwork
-from openrl.modules.networks.utils.act import ACTLayer
+from openrl.buffers.utils.util import get_shape_from_obs_space_v2
+from openrl.modules.networks.base_value_network import BaseValueNetwork
 from openrl.modules.networks.utils.cnn import CNNBase
 from openrl.modules.networks.utils.mix import MIXBase
 from openrl.modules.networks.utils.mlp import MLPBase
 from openrl.modules.networks.utils.rnn import RNNLayer
+from openrl.modules.networks.utils.util import init
 from openrl.utils.util import check_v2 as check
 
 
-class QNetwork(BasePolicyNetwork):
+class QNetwork(BaseValueNetwork):
     def __init__(
         self,
         cfg,
@@ -49,21 +50,24 @@ class QNetwork(BasePolicyNetwork):
         self.use_half = use_half
         self.tpdv = dict(dtype=torch.float32, device=device)
 
-        policy_obs_shape = get_policy_obs_space(input_space)
+        init_method = [nn.init.xavier_uniform_, nn.init.orthogonal_][
+            self._use_orthogonal
+        ]
+        obs_shape = get_shape_from_obs_space_v2(input_space)
 
-        if "Dict" in policy_obs_shape.__class__.__name__:
+        if "Dict" in obs_shape.__class__.__name__:
             self._mixed_obs = True
             self.base = MIXBase(
-                cfg, policy_obs_shape, cnn_layers_params=cfg.cnn_layers_params
+                cfg, obs_shape, cnn_layers_params=cfg.cnn_layers_params
             )
         else:
             self._mixed_obs = False
             self.base = (
-                CNNBase(cfg, policy_obs_shape)
-                if len(policy_obs_shape) == 3
+                CNNBase(cfg, obs_shape)
+                if len(obs_shape) == 3
                 else MLPBase(
                     cfg,
-                    policy_obs_shape,
+                    obs_shape,
                     use_attn_internal=cfg.use_attn_internal,
                     use_cat_self=True,
                 )
@@ -81,37 +85,28 @@ class QNetwork(BasePolicyNetwork):
             )
             input_size = self.hidden_size
 
-        self.act = ACTLayer(action_space, input_size, self._use_orthogonal, self._gain)
+        def init_(m):
+            return init(m, init_method, lambda x: nn.init.constant_(x, 0))
 
+        self.q_out = init_(nn.Linear(input_size, action_space.n))
         if use_half:
             self.half()
         self.to(device)
 
-    def forward(
-        self, raw_obs, rnn_states, masks, available_actions=None, deterministic=False
-    ):
-        policy_obs = get_policy_obs(raw_obs)
+    def forward(self, obs, rnn_states, masks):
         if self._mixed_obs:
-            for key in policy_obs.keys():
-                policy_obs[key] = check(policy_obs[key], self.use_half, self.tpdv)
-                if self.use_half:
-                    policy_obs[key].half()
+            for key in obs.keys():
+                obs[key] = check(obs[key]).to(**self.tpdv)
         else:
-            policy_obs = check(policy_obs, self.use_half, self.tpdv)
+            obs = check(obs).to(**self.tpdv)
+        rnn_states = check(rnn_states).to(**self.tpdv)
+        masks = check(masks).to(**self.tpdv)
 
-        rnn_states = check(rnn_states, self.use_half, self.tpdv)
-        masks = check(masks, self.use_half, self.tpdv)
-
-        if available_actions is not None:
-            available_actions = check(available_actions, self.use_half, self.tpdv)
-
-        actor_features = self.base(policy_obs)
+        features = self.base(obs)
 
         if self._use_naive_recurrent_policy or self._use_recurrent_policy:
-            actor_features, rnn_states = self.rnn(actor_features, rnn_states, masks)
+            features, rnn_states = self.rnn(features, rnn_states, masks)
 
-        actions, action_log_probs = self.act(
-            actor_features, available_actions, deterministic
-        )
-        return actions, action_log_probs, rnn_states
+        q_values = self.q_out(features)
 
+        return q_values, rnn_states
