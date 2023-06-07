38173b752640ae1251cef5fd909deb5c8ab6655b
unknown <yzl54d1123@163.com>
2023-06-08 01:01:12 +0800

    Successfully train dqn on gridworld

--- examples/gridworld/train_dqn_beta.py
+++ examples/gridworld/train_dqn_beta.py
@@ -10,12 +10,12 @@ from openrl.configs.config import create_config_parser
 def train():
     # 添加读取配置文件的代码
     cfg_parser = create_config_parser()
-    cfg = cfg_parser.parse_args(["--config", "dqn_cartpole.yaml"])
+    cfg = cfg_parser.parse_args(["--config", "dqn_gridworld.yaml"])
 
     # 创建 环境
     env = make(
         "GridWorldEnv",
-        env_num=1
+        env_num=9
     )
     # 创建 神经网络
     net = Net(env, cfg=cfg)
@@ -36,7 +36,7 @@ def evaluation(agent):
     step = 0
     while not np.any(done):
         # 智能体根据 observation 预测下一个动作
-        action, _ = agent.act(obs["policy"])
+        action, _ = agent.act(obs)
         obs, r, done, info = env.step(action)
         step += 1
         print(f"{step}: reward:{np.mean(r)}")
@@ -45,4 +45,4 @@ def evaluation(agent):
 
 if __name__ == "__main__":
     agent = train()
-    # evaluation(agent)
+    evaluation(agent)
