ab82d7f7a36eef870b2a4962a3f00a74ebfa7bc1
unknown <yzl54d1123@163.com>
2023-06-08 17:06:34 +0800

    fix bugs

--- examples/cartpole/train_dqn_beta.py
+++ examples/cartpole/train_dqn_beta.py
@@ -10,16 +10,16 @@ from openrl.configs.config import create_config_parser
 def train():
     # 添加读取配置文件的代码
     cfg_parser = create_config_parser()
-    cfg = cfg_parser.parse_args(["--config", "dqn_gridworld.yaml"])
+    cfg = cfg_parser.parse_args(["--config", "dqn_cartpole.yaml"])
 
     # 创建 环境
-    env = make("CartPole-v1", render_mode="group_human", env_num=1)
+    env = make("CartPole-v1", env_num=4)
     # 创建 神经网络
     net = Net(env, cfg=cfg)
     # 初始化训练器
     agent = Agent(net)
     # 开始训练
-    agent.train(total_time_steps=20000)
+    agent.train(total_time_steps=100000)
     env.close()
     return agent
 
@@ -34,7 +34,7 @@ def evaluation(agent):
     while not np.any(done):
         # 智能体根据 observation 预测下一个动作
         action, _ = agent.act(obs["policy"])
-        obs, r, done, info = env.step(action)
+        obs, r, done, info = env.step(action[0])
         step += 1
         print(f"{step}: reward:{np.mean(r)}")
     env.close()
@@ -42,4 +42,4 @@ def evaluation(agent):
 
 if __name__ == "__main__":
     agent = train()
-    # evaluation(agent)
+    evaluation(agent)
