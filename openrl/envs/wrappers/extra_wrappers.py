4c20350d291a49e6789f9d447f4ee96ddcb006d7
unknown <yzl54d1123@163.com>
2023-06-17 12:41:35 +0800

    delete cloudpickle

--- openrl/envs/wrappers/extra_wrappers.py
+++ openrl/envs/wrappers/extra_wrappers.py
@@ -89,20 +89,3 @@ class RecordReward(BaseWrapper):
     @property
     def has_auto_reset(self):
         return True
-
-
-class CloudpickleWrapper(object):
-    """
-    Uses cloudpickle to serialize contents (otherwise multiprocessing tries to use pickle)
-    """
-
-    def __init__(self, x):
-        self.x = x
-
-    def __getstate__(self):
-        import cloudpickle
-        return cloudpickle.dumps(self.x)
-
-    def __setstate__(self, ob):
-        import pickle
-        self.x = pickle.loads(ob)
