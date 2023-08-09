e27561eb96ef9be99669ec535ff6b146ca3ab111
huangshiyu <huangsy1314@163.com>
2023-08-09 18:48:10 +0800

    update

--- openrl/selfplay/wrappers/opponent_pool_wrapper.py
+++ openrl/selfplay/wrappers/opponent_pool_wrapper.py
@@ -14,11 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Play with self.
-"""
-import copy
-import time
+""""""
 from typing import List, Optional
 
 from openrl.selfplay.opponents.utils import get_opponent_from_info
