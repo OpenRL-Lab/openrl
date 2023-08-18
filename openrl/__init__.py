2fe9126cc36ea3d708254ca9890a60481022fe05
huangshiyu <huangsy1314@163.com>
2023-08-18 20:54:23 +0800

    init v0.1.3

--- openrl/__init__.py
+++ openrl/__init__.py
@@ -1,5 +1,5 @@
 __TITLE__ = "openrl"
-__VERSION__ = "v0.1.2"
+__VERSION__ = "v0.1.3"
 __DESCRIPTION__ = "Distributed Deep RL Framework"
 __AUTHOR__ = "OpenRL Contributors"
 __EMAIL__ = "huangshiyu@4paradigm.com"
